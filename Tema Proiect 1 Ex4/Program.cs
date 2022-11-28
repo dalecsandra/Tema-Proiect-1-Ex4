d2f2c54ebc60ce9469218cc61e4c470b02583c47
dascaleacalexandra <dascaleacalexandra@outlook.com>
2022-11-28 20:36:55 +0200

    Add project files.

---
+++ Tema Proiect 1 Ex4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Tema_Proiect_1_Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   Ex4
+           Scrieti un program care va afisa semnul unui numar citit de la tastatura
+           • Daca numarul este pozitiv, va afisa “+”
+           • Daca numarul este negativ, va afisa “-”
+           • Daca numarul este 0, va afisa “0”  */
+
+
+
+            //Console.WriteLine("Acest preogram va afisa semnul unui numar");
+
+            //Console.WriteLine("Introduceti numarul");
+            //int numar = int.Parse(Console.ReadLine());
+
+            int numar = 4;
+
+            if (numar > 0);
+            {
+                 Console.WriteLine("+");
+            }
+
+            else if (numar < 0);
+            {
+                Console.WriteLine("-");
+            }
+
+            else if (numar == 0);
+            {
+                Console.WriteLine("0");
+            }
+
+
+        }
+    }
+}
